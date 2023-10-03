24c46fd12c3340ddec3c239dabde6ced29bc9de4
chrisrees222 <143870938+chrisrees222@users.noreply.github.com>
2023-10-03 20:32:05 +0100

    Update get-dealership.js

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -1,89 +1,52 @@
-/**
- * Get all databases
- */
-
-const { CloudantV1 } = require('@ibm-cloud/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
-const secret = {
-    "COUCH_URL": "https://1c45f915-5eaa-4fcd-83a6-71f18e950b05-bluemix.cloudantnosqldb.appdomain.cloud",
-    "IAM_API_KEY": "8EEYtLT3A_IdcYqbWBZnK3SCSGp1ODV8V1n0185Gyusv",
-    "COUCH_USERNAME": "1c45f915-5eaa-4fcd-83a6-71f18e950b05-bluemix"
-};
-const database = 'dealerships';
-function resolveHandler(resolve, statusCode, body) {
-  resolve({
-    statusCode: statusCode,
-    headers: { 'Content-Type': 'application/json' },
-    body: body
-  });
-}
-
-function main(params) {
-  return new Promise(function (resolve, reject) {
-    const authenticator = new IamAuthenticator({ apikey: secret.IAM_API_KEY });
-    const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
-    });
-    cloudant.setServiceUrl(secret.COUCH_URL);
-    if ((params || {}).dealerId) {
-      cloudant
-        .postFind({
-          db: database,
-          selector: {
-            dealer_id: {
-              $eq: parseInt(params.dealerId)
-            }
-          }
-        })
-        .then((response) => {
-          const docs = ((response || {}).result || {}).docs || [];
-          if (docs.length === 0) {
-            resolveHandler(resolve, 404, { rows: [] });
-          } else {
-            resolveHandler(resolve, 200, { rows: docs });
-          }
-        })
-        .catch((error) => {
-          resolveHandler(resolve, 500, error);
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const Cloudant = require('@cloudant/cloudant');
+// Initialize Cloudant connection with IAM authentication
+async function dbCloudantConnect() {
+    try {
+        const cloudant = Cloudant({
+            plugins: { iamauth: { iamApiKey: 'EuqgDNyFwOekmX9A1WtAx46DNZtP5XDeRlhhaleHRTnB' } }, // Replace with your IAM API key
+            url: 'https://603c7b14-ff01-4ca5-b02e-f1066525c67d-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
         });
-    } else if ((params || {}).state) {
-      cloudant
-        .postFind({
-          db: database,
-          selector: {
-            state: {
-              $eq: params.state
-            }
-          }
-        })
-        .then((response) => {
-          const docs = ((response || {}).result || {}).docs || [];
-          if (docs.length === 0) {
-            resolveHandler(resolve, 404, { rows: [] });
-          } else {
-            resolveHandler(resolve, 200, { rows: docs });
-          }
-        })
-        .catch((error) => {
-          resolveHandler(resolve, 500, error);
-        });
-    } else {
-      cloudant
-        .postAllDocs({
-          db: database,
-          includeDocs: true
-        })
-        .then((response) => {
-          const docs = ((response || {}).result || {}).rows || [];
-          if (docs.length === 0) {
-            resolveHandler(resolve, 404, { rows: [] });
-          } else {
-            resolveHandler(resolve, 200, { rows: docs });
-          }
-        })
-        .catch((error) => {
-          resolveHandler(resolve, 500, error);
-        });
+        const db = cloudant.use('dealerships');
+        console.info('Connect success! Connected to DB');
+        return db;
+    } catch (err) {
+        console.error('Connect failure: ' + err.message + ' for Cloudant DB');
+        throw err;
     }
-  });
 }
+let db;
+(async () => {
+    db = await dbCloudantConnect();
+})();
+app.use(express.json());
+// Define a route to get all dealerships with optional state and ID filters
+app.get('/dealerships/get', (req, res) => {
+    const { state, id } = req.query;
+    // Create a selector object based on query parameters
+    const selector = {};
+    if (state) {
+        selector.state = state;
+    }
+    if (id) {
+        selector._id = id;
+    }
+    const queryOptions = {
+        selector,
+        limit: 10, // Limit the number of documents returned to 10
+    };
+    db.find(queryOptions, (err, body) => {
+        if (err) {
+            console.error('Error fetching dealerships:', err);
+            res.status(500).json({ error: 'An error occurred while fetching dealerships.' });
+        } else {
+            const dealerships = body.docs;
+            res.json(dealerships);
+        }
+    });
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
